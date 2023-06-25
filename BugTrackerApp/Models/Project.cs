d556795d3d89488adc1b28404e838382fc1d34a7
alexnguyen1994 <alexnguyen1994@csu.fullerton.edu>
2023-06-24 19:58:15 -0700

    Project model added

---
+++ BugTrackerApp/Models/Project.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+
+namespace BugTrackerApp.Models
+{
+    public class Project
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+
+        // tickets and users linked to the project
+        //public ICollection<Ticket> Tickets { get; set; }
+        //public ICollection<IdentityUser> Users { get; set; }
+    }
+}
