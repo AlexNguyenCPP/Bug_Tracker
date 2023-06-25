4aaa35a542c972933acc5e9162618c3f37dc004e
alexnguyen1994 <alexnguyen1994@csu.fullerton.edu>
2023-06-24 20:11:39 -0700

    "Project" model pushed to database

--- BugTrackerApp/Data/ApplicationDbContext.cs
+++ BugTrackerApp/Data/ApplicationDbContext.cs
@@ -1,13 +1,16 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BugTrackerApp.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BugTrackerApp.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {
         }
+
+        // Project model
+        public DbSet<Project> Projects { get; set; }
     }
 }
