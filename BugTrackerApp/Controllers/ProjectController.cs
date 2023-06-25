35e7cabc3093370aea72cd136b982279f29e5ecf
alexnguyen1994 <alexnguyen1994@csu.fullerton.edu>
2023-06-24 23:47:43 -0700

    Project controller and view added

---
+++ BugTrackerApp/Controllers/ProjectController.cs
@@ -0,0 +1,22 @@
+﻿using BugTrackerApp.Data;
+using BugTrackerApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BugTrackerApp.Controllers
+{
+    public class ProjectController : Controller
+    {
+        // retrieve project list
+        private readonly ApplicationDbContext _db;
+        public ProjectController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Project> objProjectList = _db.Projects;
+            return View(objProjectList);
+        }
+    }
+}
