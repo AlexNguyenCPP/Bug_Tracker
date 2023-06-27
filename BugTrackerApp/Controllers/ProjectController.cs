353108011c8ae8d7b71e4ed8377da46220d90839
alexnguyen1994 <alexnguyen1994@csu.fullerton.edu>
2023-06-26 22:20:13 -0700

    server side validation added for "create project"

--- BugTrackerApp/Controllers/ProjectController.cs
+++ BugTrackerApp/Controllers/ProjectController.cs
@@ -30,10 +30,22 @@ namespace BugTrackerApp.Controllers
         [ValidateAntiForgeryToken] //prevent cross-site request forgery. Not required but recommended
         public IActionResult Create(Project obj)
         {
-            _db.Projects.Add(obj);
-            _db.SaveChanges();
-            return RedirectToAction("Index");// if returning to an action method inside a different controller, do this
-                                             // return RedirectToAction("Index", "<controller-name");
+            // make sure the description and the name are not the same
+            if (obj.Name == obj.Description)
+            {
+                ModelState.AddModelError("Name", "The name cannot match the description");
+            }
+
+            // server side validation
+            if (ModelState.IsValid)
+            {
+                // add the created project to the database
+                _db.Projects.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");// if returning to an action method inside a different controller, do this
+            }                                    // return RedirectToAction("Index", "<controller-name");
+
+            return View(obj);
         }
     }
 }
