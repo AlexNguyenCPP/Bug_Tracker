d543122196c7fdc30bc7f8b6b9fa94aa1d69bf4c
alexnguyen1994 <alexnguyen1994@csu.fullerton.edu>
2023-06-27 22:19:30 -0700

    Ticket model created

---
+++ BugTrackerApp/Models/Ticket.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BugTrackerApp.Models
+{
+    public class Ticket
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int ProjectId { get; set; } // Foreign key property
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+        public string Developer { get; set; }
+        public string Priority { get; set; }
+        public string Status { get; set; }
+
+        [ForeignKey("ProjectId")] // Foreign key for 'Project'
+        public Project Project { get; set; } // Navigation property
+
+        public DateTime Created { get; set; }
+    }
+
+}
